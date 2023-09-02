d7973de6992f70c9cf547e05af8b50e9089567c9
TechHunter <lyzethan@gmail.com>
2023-09-02 16:50:44 +0800

    Update Hunter Library Rayfield Loader.lua

---
+++ Hunter Library/Rayfield UI Version/Hunter Library Rayfield Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 286090429 then loadstring(game:HttpGet((""),true))() else --arsenal--
+if game.PlaceId == 185655149 then loadstring(game:HttpGet((""),true))() else -- bloxburg --
+if game.PlaceId == 5670292785 then loadstring(game:HttpGet((""),true))() else -- Undead defense --
+if game.PlaceId == 11701792069 then loadstring(game:HttpGet((""),true))() else -- snow plow --
+if game.PlaceId == 13622981808 then loadstring(game:HttpGet((""),true))() --[[ rb friends]] else loadstring(game:HttpGet(("").true))() -- ninja legends --
