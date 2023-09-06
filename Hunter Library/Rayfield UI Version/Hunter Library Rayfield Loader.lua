e2f0a4d74f5593803dc226792979d9f5a04e2ab1
TechHunter <lyzethan@gmail.com>
2023-09-06 17:19:17 +0800

    Update Hunter Library Rayfield Loader.lua

--- Hunter Library/Rayfield UI Version/Hunter Library Rayfield Loader.lua
+++ Hunter Library/Rayfield UI Version/Hunter Library Rayfield Loader.lua
@@ -5,7 +5,7 @@ if game.PlaceId == 286090429 then
       loadstring(game:HttpGet((""),true))() 
     else -- bloxburg --
   if game.PlaceId == 5670292785 then 
-        loadstring(game:HttpGet((""),true))()
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/MrKillingHunter/Hunter-Scripts/main/Hunter%20Library/Rayfield%20UI%20Version/Undead%20Defense%20Tycoon.lua"),true))()
       else -- Undead defense --
   if game.PlaceId == 11701792069 then
           loadstring(game:HttpGet(("https://raw.githubusercontent.com/MrKillingHunter/Hunter-Scripts/main/Hunter%20Library/Rayfield%20UI%20Version/Snow%20Plow%20Simulator.lua"),true))()
