06feb1be006c1daf5378b61d42c21fe8f7be967e
TechHunter <lyzethan@gmail.com>
2023-09-23 16:08:12 +0800

    Delete Hunter Library directory

--- Hunter Library/Kavo UI Version/Colors.lua
+++
@@ -1,9 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local colors = {
-    SchemeColor  = Color3.fromRGB(48, 118, 112),
-    Background   = Color3.fromRGB(15, 15, 15),
-    Header       = Color3.fromRGB(15, 15, 15),
-    TextColor    = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(46, 85, 83)
-}
-local Window = Library.CreateLib("Hunter Library (v1.1) - Game", colors)
